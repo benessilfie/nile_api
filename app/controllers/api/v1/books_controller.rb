859dde13f8cac0afab2b633c23c61baaddd7e2ca
essilfiequansah <bquansah007@gmail.com>
2022-09-26 06:19:30 +0000

    Add create action to the books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, staus: :created
+    else
+      render json: book.errors, staus: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
 end
